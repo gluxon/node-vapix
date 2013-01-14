3ac568c30b47ae5ef8f977f9824d3b4f88ca4a48
Brandon Cheng <bcheng.gt@gmail.com>
2013-01-14 15:47:25 -0500

    Basic description

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+/**
+ * VAPIX is Axis’ own open application programming interface (API) for
+ * cost-efficient, flexible, scalable and future-proof integration with
+ * other systems. 
+ */
+
 module.exports = require('./lib/vapix');
