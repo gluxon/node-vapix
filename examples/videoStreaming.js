9adf500f342f99eed31fd5931bf9beab88703879
Enforcers <enforcers@ubuntu.(none)>
2013-03-14 01:08:37 -0400

    Bump to 0.5

--- examples/videoStreaming.js
+++ examples/videoStreaming.js
@@ -13,20 +13,22 @@ var camera = new vapix.Camera(options);
 var options = {
 	resolution: '640x480',
 	compression: 25,
-	duration: 10,
-	fps: 30
+	duration: 30,
+	fps: 10
 }
 
 var mjpg = camera.createVideoStream(options);
 
 var counter = 1;
 
-fs.mkdir('frames');
+fs.mkdir('frames', function(err) {
+	console.err('Please remove the frames folder before re-running this example.');
+});
 
 mjpg.on('data', function(data) {
-	fs.writeFile('frames/' + counter + ".jpg", data, function(err) {
+	/*fs.writeFile('frames/' + counter + ".jpg", data, function(err) {
 		if (err) throw err;
-	});
+	});*/
 
 	counter++;
 });
